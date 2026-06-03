c922ad51969f123798d4a005935c77d9908ec2b1
pabin-limbu <changsu.pabin@gmail.com>
2026-06-03 18:38:51 +0800

    fixing environment variable

--- src/app/(dashboard)/test/page.tsx
+++ src/app/(dashboard)/test/page.tsx
@@ -1,19 +1,10 @@
-import React, { Suspense } from "react";
-import { HealthCheck } from "./health-check";
-import { HydrateClient } from "@/trpc/server";
-import { ErrorBoundary } from "react-error-boundary";
+import React from "react";
 
 function Page() {
   return (
-    <HydrateClient>
-      <div className="flex flex-col">
-        <ErrorBoundary fallback={<div>Something went wrong.</div>}>
-          <Suspense fallback={<div>loading......</div>}>
-            <HealthCheck />
-          </Suspense>
-        </ErrorBoundary>
-      </div>
-    </HydrateClient>
+    <div className="flex flex-col">
+      <p>hi</p>
+    </div>
   );
 }
 
